ae735636a3df55b6c8bb626ec951f2666a0a70b5
Mateus Trindade <mateustpimenta@gmail.com>
2020-10-01 11:48:48 -0300

    React JS
    uploading React Js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="rightSide">
+          <h1>
+            Main Videos 
+          </h1>
+        </div>
+        <div id="searchLog">
+          <input id="inputForSearch">
+          </input>
+        </div>
+        <div id="usersDiv">
+          <h1>
+            users
+          </h1>
+        </div>
+
+
+ 
       </header>
+      <main>
+
+      </main>
     </div>
   );
 }
